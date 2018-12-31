00d02fafcb3b2353ae8932a48124982ad4df0583
PuaChris <chris.pua@mail.utoronto.ca>
2018-12-31 10:55:12 -0500

    Created individual item layout of word list

---
+++ SAT Vocab Buddy/src/main/java/puachris/satstudybuddy/MainScreen.java
@@ -0,0 +1,20 @@
+package puachris.satstudybuddy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+    }
+
+}
