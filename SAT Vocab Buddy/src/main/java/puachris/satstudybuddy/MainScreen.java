506e233adffca808387d34cf0c0711019ff30d95
PuaChris <chris.pua@mail.utoronto.ca>
2019-01-01 13:27:57 -0500

    Created layout for vocabulary practice

--- SAT Vocab Buddy/src/main/java/puachris/satstudybuddy/MainScreen.java
+++ SAT Vocab Buddy/src/main/java/puachris/satstudybuddy/MainScreen.java
@@ -1,12 +1,8 @@
 package puachris.satstudybuddy;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,24 +12,24 @@ public class MainScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.vocab_package_list);
 
         ListView mListView = (ListView) findViewById(R.id.listView);
 
 
         // Create Vocab_Packages
-        Vocab_Package test = new Vocab_Package(1, 0, 10);
-        Vocab_Package hello = new Vocab_Package(2, 3, 7);
-        Vocab_Package hi = new Vocab_Package(3, 17, 17);
+        VocabPackage test = new VocabPackage(1, 0, 10);
+        VocabPackage hello = new VocabPackage(2, 3, 7);
+        VocabPackage hi = new VocabPackage(3, 17, 17);
 
 
-        // Array to hold all the Vocab_Package objects
-        ArrayList<Vocab_Package> vocabList = new ArrayList<Vocab_Package>();
+        // Array to hold all the VocabPackage objects
+        ArrayList<VocabPackage> vocabList = new ArrayList<VocabPackage>();
         vocabList.add(test);
         vocabList.add(hello);
         vocabList.add(hi);
 
-        VocabListAdapter adapter = new VocabListAdapter(this, R.layout.adapter_view_layout, vocabList);
+        VocabListAdapter adapter = new VocabListAdapter(this, R.layout.vocab_package, vocabList);
         mListView.setAdapter(adapter);
 
     }
