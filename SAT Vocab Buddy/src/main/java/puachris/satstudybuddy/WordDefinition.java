01a03a4fba497e85eabf18b4dcdf69e369780457
PuaChris <chris.pua@mail.utoronto.ca>
2019-01-04 12:10:15 -0500

    Adding WordDefinition class

---
+++ SAT Vocab Buddy/src/main/java/puachris/satstudybuddy/WordDefinition.java
@@ -0,0 +1,41 @@
+package puachris.satstudybuddy;
+
+import java.io.Serializable;
+
+/*** OBJECT TO HOLD THE WORD ITSELF, AN EXAMPLE USAGE OF THE WORD, AND ITS DEFINITION ***/
+
+public class WordDefinition implements Serializable {
+    private String word;
+    private String example;
+    private String definition;
+
+    public WordDefinition(String word, String example, String definition) {
+        this.word = word;
+        this.example = example;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
